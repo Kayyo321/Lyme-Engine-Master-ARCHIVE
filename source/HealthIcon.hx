93b3f6fb8b5f0b6775a9ce52ee90b7db39e13df9
SLBB2021 <76451541+SLBB2021@users.noreply.github.com>
2021-08-06 15:52:02 -0400

    Updated HealthIcon
    more readable

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -15,25 +15,68 @@ class HealthIcon extends FlxSprite
 		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 
 		antialiasing = true;
+
+		// global 
+
 		animation.add('bf', [0, 1], 0, false, isPlayer);
-		animation.add('bf-car', [0, 1], 0, false, isPlayer);
-		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
-		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
-		animation.add('spooky', [2, 3], 0, false, isPlayer);
-		animation.add('pico', [4, 5], 0, false, isPlayer);
-		animation.add('mom', [6, 7], 0, false, isPlayer);
-		animation.add('mom-car', [6, 7], 0, false, isPlayer);
-		animation.add('tankman', [8, 9], 0, false, isPlayer);
+	  //------------------------------------------------	
 		animation.add('face', [10, 11], 0, false, isPlayer);
-		animation.add('dad', [12, 13], 0, false, isPlayer);
-		animation.add('senpai', [22, 22], 0, false, isPlayer);
-		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
-		animation.add('spirit', [23, 23], 0, false, isPlayer);
+	  //----------------------------------------------------
 		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+	  //------------------------------------------------------
+
+		// bf versions 
+
+		animation.add('bf-car', [0, 1], 0, false, isPlayer);
+      //----------------------------------------------------
+		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
+      //----------------------------------------------------------
+		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
+      //--------------------------------------------------------
+
+		// gf versions
+
 		animation.add('gf', [16], 0, false, isPlayer);
-		animation.add('parents-christmas', [17], 0, false, isPlayer);
+
+		// week 1
+
+		animation.add('dad', [12, 13], 0, false, isPlayer);
+
+		// week 2
+
 		animation.add('monster', [19, 20], 0, false, isPlayer);
+      //-------------------------------------------------------
+		animation.add('spooky', [2, 3], 0, false, isPlayer);
+
+		// week 3
+		
+		animation.add('pico', [4, 5], 0, false, isPlayer);
+
+		// week 4
+
+		animation.add('mom', [6, 7], 0, false, isPlayer);
+      //-------------------------------------------------
+		animation.add('mom-car', [6, 7], 0, false, isPlayer);
+      //-----------------------------------------------------
+		
+		// week 5
+
+		animation.add('parents-christmas', [17], 0, false, isPlayer);
+      //-------------------------------------------------------------		
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		
+		// week 6
+
+		animation.add('senpai', [22, 22], 0, false, isPlayer);
+      //------------------------------------------------------	
+		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
+      //------------------------------------------------------------		
+		animation.add('spirit', [23, 23], 0, false, isPlayer);
+
+		// week 7
+		
+		animation.add('tankman', [8, 9], 0, false, isPlayer);
+
 		animation.play(char);
 		scrollFactor.set();
 	}
